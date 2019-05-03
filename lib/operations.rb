db04bd494732b17b1957a2e43848a03c6a413302
Abagail Hudson <abagailanne@gmail.com>
2019-05-03 18:50:41 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,7 @@
 def unsafe?(speed)
- if  !speed.between?(40,60)
-	 return true
- else
-	 return false
- end
+ !speed.between?(40,60)
 end
 
-
-
 def not_safe?(speed)
-
+ speed.between?(40,60) ? true : false
 end
